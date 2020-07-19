08411acfeff1738f0fc7beeb236c0a1deda1c989
Rogelio <molekilla@gmail.com>
2020-07-19 17:02:06 -0500

    sockjs

--- src/views/dashboard/pages/shared/SmartCardConnector.ts
+++ src/views/dashboard/pages/shared/SmartCardConnector.ts
@@ -1,3 +1,4 @@
+import * as SockJS from 'sockjs-client';
 const os = require('os');
 const axios = require('axios');
 
@@ -8,18 +9,21 @@ export interface SmartCardConnectorEvent {
 const CLIENT_API = 'http://localhost:8089';
 export class SmartCardConnectorPKCS11 {
     module: string;
+    stompClient: any;
     constructor(private keyId?: string) {
         this.keyId = keyId;
-
+        this.connect();
         if (os.platform() === 'linux') {
             this.module = '/usr/lib/libaetpkss.so';
         } else if (os.platform() === 'darwin') {
             // 
             this.module = '/usr/local/lib/libaetpkss.dylib';
-        } else if  (os.platform() === 'win32') {
+        } else if (os.platform() === 'win32') {
             // 
             this.module = `C:/\Windows/\SysWOW64/\aetpkss1.dll`;
         }
+
+
     }
 
 
@@ -33,9 +37,31 @@ export class SmartCardConnectorPKCS11 {
     async sign(index: string, pin: string, data: Buffer) {
         const res = await axios.post(`${CLIENT_API}/sc/sign/${index}`, {
             pin,
-          data:  data.toString('base64')
+            data: data.toString('base64')
         });
         return res.data;
+    }
+
+    connect() {
+        var socket = new SockJS(CLIENT_API + '/ws');
+        
+        this.stompClient = socket
+        this.stompClient.connect({}, (frame) => {
+            debugger
+            // setConnected(true);
+            // console.log('Connected: ' + frame);
+            // this.stompClient.subscribe('/topic/greetings', (greeting) => {
+            //     showGreeting(JSON.parse(greeting.body).content);
+            // });
+        });
+    }
+
+    disconnect() {
+        if (this.stompClient !== null) {
+            this.stompClient.disconnect();
+        }
+    //    setConnected(false);
+        console.log("Disconnected");
     }
 
 }
