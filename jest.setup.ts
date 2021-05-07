f3da6d1ba3cef2beea606a1386491cf0345da88c
Roberto E. Zubieta <roberto.zubieta@admios-sa.com>
2021-05-07 10:30:54 -0500

    Timezone Tests Fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import moment from "moment-timezone";
 
 // Vuetify Fix: https://github.com/vuetifyjs/vuetify/discussions/4068#discussioncomment-24984
 Vue.use(Vuetify);
+
+// MomentJS Fix
+moment.tz.setDefault("UTC");
