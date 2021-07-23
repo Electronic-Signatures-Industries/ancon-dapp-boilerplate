5e1d30bb7665c39e73d34cb76312f2d756bc91e9
Rogelio Morrell <molekilla@gmail.com>
2021-07-22 21:33:31 -0500

    XDV Documents 3 , RC 2.0

--- src/layouts/full-layout/footer/Footer.vue
+++ src/layouts/full-layout/footer/Footer.vue
@@ -3,7 +3,7 @@
     <v-col cols="12">
       <span
         >© Industrias de Firmas Electronicas SA,
-        {{ new Date().getFullYear() }} — https://ifesa.tech/ - XDV v3.0-rc.1 -
+        {{ new Date().getFullYear() }} — https://ifesa.tech/ - XDV v3.0-rc.2 -
         Made with <v-icon small color="red"> mdi-heart </v-icon> in Panama
       </span>
     </v-col>
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import store from './store';
 import Vue from 'vue';
 import VueSkycons from 'vue-skycons';
 import vuetify from './plugins/vuetify';
-import VueWorker from 'vue-worker';
 import './plugins/base';
 import './plugins/chartist';
 import './plugins/vee-validate';
 import './plugins/appconfig';
 import Vuebar from 'vuebar';
 
+
 import GAuth from 'vue-google-oauth2'
+import Cryptoicon from 'vue-cryptoicon';
+// For all icons
+import icon from 'vue-cryptoicon/src/icons';
+Cryptoicon.add(icon);
+Vue.use(Cryptoicon);
+
 const gauthOption = {
   clientId: '779014570531-bvmnb5bq69uov2cc1vkr4jhh03pgdrvj.apps.googleusercontent.com',
   scope: 'profile email',
@@ -25,7 +31,7 @@ Vue.use(VueSkycons, {
 });
 
 Vue.use(Vuebar);
-Vue.use(VueWorker)
+// Vue.use(VueWorker)
 Vue.config.productionTip = true;
 
 
