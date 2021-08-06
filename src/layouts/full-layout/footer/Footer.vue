07267febba8dba0cc02724a21997ca1240e8acb3
Kendall Kant <kantkendall@gmail.com>
2021-08-06 10:58:10 -0500

    👌 IMPROVE: Footer alignment

--- src/layouts/full-layout/footer/Footer.vue
+++ src/layouts/full-layout/footer/Footer.vue
@@ -1,15 +1,23 @@
 <template>
   <v-footer app class="footerpart" inset>
-    <v-col cols="12">
-      <v-btn href="mailto:info@ifesa.tech" text offset="10">Inquiry business solutions</v-btn>
-      <br/>
-      <br/>
-      <span
-        >© Industrias de Firmas Electronicas SA,
-        {{ new Date().getFullYear() }} <br/> https://ifesa.tech/ - XDV v3.0 
-        Made with <v-icon small color="red"> mdi-heart </v-icon> in Panama
-      </span>
-    </v-col>
+    <v-row justify="space-between">
+      <v-col md="6">
+        <span
+          >© Industrias de Firmas Electronicas SA,
+          {{ new Date().getFullYear() }} <br />
+          XDV v3.0 Made with
+          <v-icon small color="red"> mdi-heart </v-icon> in Panama
+        </span>
+      </v-col>
+      <v-col md="6" align="right">
+        <v-btn href="mailto:info@ifesa.tech" text offset="10"
+          > Inquiry business solutions</v-btn
+        >
+        <v-btn href="mailto:info@ifesa.tech" text offset="10"
+          > Support</v-btn
+        >
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 <script>
